c56d3fad28eed98bb35e204be816da9ae048ab61
Alena Gushcha <g.aliena@mail.ru>
2026-03-27 13:21:54 +0300

    added ui test

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope='function')
+def setup_browser():
+    options = Options()
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    # driver = webdriver.Chrome(options=options)
+
+    yield driver
