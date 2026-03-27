9b121c930a0009734ce690cbd3ed312218b3596a
Alena Gushcha <g.aliena@mail.ru>
2026-03-27 17:08:04 +0300

    added chrome version

--- conftest.py
+++ conftest.py
@@ -5,12 +5,17 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def setup_browser():
     options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "127.0")
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": False
+    })
 
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    # driver = webdriver.Chrome(options=options)
-
     yield driver
+    driver.quit()
